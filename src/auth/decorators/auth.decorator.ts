e3e5f0622b1a62dbc2d94875ddffadc5de28086f
ratnov2 <73044570+ratnov2@users.noreply.github.com>
2023-04-21 00:37:56 +0300

    fix

--- src/auth/decorators/auth.decorator.ts
+++ src/auth/decorators/auth.decorator.ts
@@ -3,8 +3,10 @@ import { TypeRole } from '../auth.interface'
 import { OnlyAdminGuard } from '../guards/admin.guard'
 import { JwtAuthGuard } from '../guards/jwt.guard'
 
+
 export function Auth(role: TypeRole = 'user') {
 	return applyDecorators(
+		//
 		role === 'admin'
 			? UseGuards(JwtAuthGuard, OnlyAdminGuard)
 			: UseGuards(JwtAuthGuard)
