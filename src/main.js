310622c0b72c5a763292590ea1806cdd81c7b1d7
ratnov2 <73044570+ratnov2@users.noreply.github.com>
2023-04-21 03:27:34 +0300

    ffg

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
+	console.log('weflnweflnwefl');
 	const app = await NestFactory.create(AppModule)
 	app.setGlobalPrefix('api')
+
 	app.enableCors();
 	await app.listen(4200)
 }
