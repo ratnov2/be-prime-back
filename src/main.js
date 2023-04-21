b431d23580fb47ee7268d4b156e1febaa93f5de7
ratnov2 <73044570+ratnov2@users.noreply.github.com>
2023-04-21 03:50:31 +0300

    fef

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-	console.log('weflnweflnwefl');
 	const app = await NestFactory.create(AppModule)
 	app.setGlobalPrefix('api')
 
